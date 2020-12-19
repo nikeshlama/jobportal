34168bf23e6b913dfb2103af49e049aef6fee60a
Sudeep Bhattarai <sudeepbhattarai100@gmail.com>
2020-12-19 13:31:16 +0545

    Reset password and delete user testing

---
+++ app/src/test/java/com/sudeep/jobportal/TDDRegistrationtesting.java
@@ -0,0 +1,13 @@
+package com.sudeep.jobportal;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
---
+++ app/src/test/java/com/sudeep/jobportal/TDDResetPasswordTesting.java
@@ -0,0 +1,31 @@
+package com.sudeep.jobportal;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class TDDResetPasswordTesting {
+    @Test
+    public void ResetPasswordTestin(){
+        FirebaseAuth auth = Mockito.mock(FirebaseAuth.class);
+        final Task<Void> mockedAuth = Mockito.mock(Task.class);
+        when(auth.sendPasswordResetEmail("sudeeptddtesting@gmail.com"))
+                .thenReturn(mockedAuth);
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Task<Void> authResult = invocation.getArgument(0,Task.class);
+                assertEquals(true,authResult.isSuccessful());
+                return null;
+            }
+        });
+
+    }
+}
