a8fdbd7a31167170156ed91dfd484ddbdfe37715
Dell <angelbhandari445@gmail.com>
2020-12-27 20:17:48 +0545

    Dashboard complited

--- app/src/main/java/com/nikesh/jobportal/MainActivity.java
+++ app/src/main/java/com/nikesh/jobportal/MainActivity.java
@@ -1,30 +1,62 @@
 package com.nikesh.jobportal;
 
-import android.os.Bundle;
+import androidx.annotation.NonNull;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+public class MainActivity{
+    public void FollowUser(final String userId, final String FollowingUserId)
+    {
 
-public class MainActivity extends AppCompatActivity {
+        final DatabaseReference followRef = FirebaseDatabase.getInstance().getReference("Follow")
+                .child(userId)
+                .child("Following")
+                .child(FollowingUserId);
+        followRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.exists())
+                {
+                    followRef.child("id").setValue(FollowingUserId);
+                }
+            }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate (savedInstanceState);
-        setContentView (R.layout.activity_main);
-        BottomNavigationView navView = findViewById (R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder (
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build ();
-        NavController navController = Navigation.findNavController (this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController (this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController (navView, navController);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        final DatabaseReference followRef2 = FirebaseDatabase.getInstance().getReference("Follow")
+                .child(FollowingUserId)
+                .child("Followers")
+                .child(userId);
+        followRef2.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.exists())
+                {
+                    followRef2.child("id").setValue(userId);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    public void RemoveFollow(String userID , String FollowingUserId)
+    {
+
+        FirebaseDatabase.getInstance().getReference("Follow").child(userID).child("Following").child(FollowingUserId).removeValue();
+        FirebaseDatabase.getInstance().getReference("Follow").child(FollowingUserId).child("Followers").child(userID).removeValue();
+
     }
 
 }
