627d858855389549c391ac34255f9321c275f160
nikeshlama <nikeshlama76@gmail.com>
2021-01-12 17:10:58 +0545

    comment activity added with comment adapter

---
+++ app/src/main/java/com/nikesh/jobportal/Activity/CommentActivity.java
@@ -0,0 +1,178 @@
+package com.nikesh.jobportal.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.nikesh.jobportal.Adapter.CommentAdapter;
+import com.nikesh.jobportal.Model.Comment;
+import com.nikesh.jobportal.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class CommentActivity extends AppCompatActivity {
+
+
+
+    TextView like, noComments;
+    EditText commentBox;
+    ImageView btnLike,sendComment;
+    RecyclerView commentRecyclerView;
+    List<Comment>  comments;
+    FirebaseUser firebaseUser;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comment);
+        getSupportActionBar().setTitle("Comment ");
+        final String postId = getIntent().getStringExtra("PostId");
+        like= findViewById(R.id.textViewLikeCount);
+        btnLike= findViewById(R.id.btnLike2);
+        noComments= findViewById(R.id.NoComments);
+        commentBox= findViewById(R.id.commentBox);
+        sendComment= findViewById(R.id.sendComment);
+        commentRecyclerView= findViewById(R.id.commentRecyclerView);
+        commentRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        getSupportActionBar().show();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        readLikes(postId);
+        readComments(postId);
+        CheckLike(postId,btnLike);
+        sendComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendComments(postId);
+            }
+        });
+        btnLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(btnLike.getTag().equals("Like"))
+                {
+                    FirebaseDatabase.getInstance().getReference("Activities").child(postId).child("Like").child(firebaseUser.getUid()).setValue(true);
+
+                }
+                else if (btnLike.getTag().equals("Liked"))
+                {
+                    FirebaseDatabase.getInstance().getReference("Activities").child(postId).child("Like").child(firebaseUser.getUid()).removeValue();
+                }
+            }
+        });
+    }
+
+    private void readLikes(String postId)
+    {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Activities").child(postId).child("Like");
+        reference.addValueEventListener(new ValueEventListener () {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                like.setText(dataSnapshot.getChildrenCount()+" likes");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    private void readComments(String PostId)
+    {
+        commentRecyclerView.setVisibility(View.VISIBLE);
+        noComments.setVisibility(View.GONE);
+        comments = new ArrayList<>();
+        comments.clear();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Activities").child(PostId).child("Comment");
+        reference.addValueEventListener(new ValueEventListener () {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren())
+                {
+                    Comment comment = dataSnapshot1.getValue(Comment.class);
+                    comments.add(comment);
+                }
+                Collections.reverse(comments);
+                if (comments.size()==0)
+                {
+                    commentRecyclerView.setVisibility(View.GONE);
+                    noComments.setVisibility(View.VISIBLE);
+
+                }
+                else
+                {
+                    CommentAdapter commentAdapter = new CommentAdapter(comments,getApplicationContext());
+                    commentRecyclerView.setAdapter(commentAdapter);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    private void sendComments(String postId)
+    {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Activities").child(postId).child("Comment");
+        String commentId = reference.push().getKey();
+        String comment = commentBox.getText().toString();
+        if (comment.equals(""))
+        {
+            Toast.makeText(this, "Comment box is empty", Toast.LENGTH_SHORT).show();
+        }else
+        {
+            HashMap hashMap = new HashMap();
+            hashMap.put("id",commentId);
+            hashMap.put("comment",comment);
+            hashMap.put("userId",firebaseUser.getUid());
+            reference.push().setValue(hashMap);
+
+            Toast.makeText(this, "Comment send", Toast.LENGTH_SHORT).show();
+            commentBox.setText("");
+        }
+
+    }
+    private void CheckLike(final String eventId, final ImageView imageView  ) {
+        final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Activities").child(eventId).child("Like");
+        reference.addValueEventListener(new ValueEventListener () {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if (dataSnapshot.child(firebaseUser.getUid()).exists()) {
+                    imageView.setImageResource(R.drawable.ic_baseline_blue0thumb_up_24);
+                    imageView.setTag("Liked");
+                } else {
+                    imageView.setImageResource(R.drawable.ic_baseline_thumb_up_24);
+                    imageView.setTag("Like");
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/nikesh/jobportal/ui/home/HomeFragment.java
+++ app/src/main/java/com/nikesh/jobportal/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
@@ -17,21 +19,83 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
+import com.nikesh.jobportal.Adapter.HomeAdapter;
+import com.nikesh.jobportal.Model.Events;
+import com.nikesh.jobportal.Model.Following;
 import com.nikesh.jobportal.R;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class HomeFragment extends Fragment {
+
+
+    RecyclerView recyclerView;
+
+
+
+    List<Events> events;
+    DatabaseReference reference;
+    FirebaseUser firebaseUser;
+    HomeAdapter homeAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    ProgressBar progressBar;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        recyclerView = root.findViewById(R.id.homeRecycleView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        events = new ArrayList<>();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        progressBar = root.findViewById(R.id.progressbarRecycleView);
 
+        readEvents();
         return root;
     }
+    public void readEvents()
+    {
+        progressBar.setVisibility(View.VISIBLE);
+
+        reference = FirebaseDatabase.getInstance().getReference("Events");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren())
+                {
+                    Events event = dataSnapshot1.getValue(Events.class);
+                    events.add(event);
+                    progressBar.setVisibility(View.GONE);
+
+                }
+                Collections.reverse(events);
+                homeAdapter = new HomeAdapter(getContext(),events,firebaseUser.getUid());
+                recyclerView.setAdapter(homeAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
+        progressBar.setVisibility(View.GONE);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
 }
